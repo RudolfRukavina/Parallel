a6da89483821f26816066315b23f6e49d71ff80e
RudolfRukavina <rukavina.rudi@gmail.com>
2024-01-26 12:32:44 +0100

    v0.8

--- src/components/Hero2.vue
+++ src/components/Hero2.vue
@@ -15,9 +15,7 @@
       <h1 class="mb-4 text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">
         Poduke Parallel</h1>
       <p class="mb-8 text-lg font-normal text-gray-600 lg:text-xl sm:px-16 xl:px-48 ">
-
-        Dobrodošli u Parallel poduke. Naš cilj je olakšati učenje kroz jedinstven
-        pristup dijeljenja iskustva i znanja, podržavajući druge u njihovom obrazovnom putovanju.
+        Osvježite svoje učenje kroz naše iskustvo, poticaj i konstantnu podršku.
       </p>
       <div class="flex flex-col mb-6 lg:mb-8 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
         <RouterLink to='/onama'
